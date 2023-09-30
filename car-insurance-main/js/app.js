fd6d44d22dfb8cccfeb16116b44352618bd0bd4c
Mahsa Arabzadeh <arabzadehmah@gmail.com>
2023-09-30 13:16:53 +0330

    add current year
    add function that can get current year and 20 past year

--- car-insurance-main/js/app.js
+++ car-insurance-main/js/app.js
@@ -1,3 +1,17 @@
-// get current year.
-const year = new Date().toLocaleDateString("fa-IR");
-console.log(year);
+function convertToJalali(georgianYear) {
+  // Algorithm to convert Gregorian year to Jalali (Shamsi) year
+  const jalaliYear = ((georgianYear - 621) * 33) / 32;
+  return Math.floor(jalaliYear);
+}
+
+function displayPast20JalaliYears() {
+  // Get the current year in the Jalali calendar system
+  const currentYear = convertToJalali(new Date().getFullYear());
+
+  // Calculate Jalali years from 20 years ago to the current year
+  for (let i = currentYear - 20; i <= currentYear; i++) {
+    console.log(i);
+  }
+}
+// Display 20 years from 1382 to 1402 in the Jalali calendar format
+displayPast20JalaliYears();
