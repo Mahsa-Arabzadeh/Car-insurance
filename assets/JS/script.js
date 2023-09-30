860be23bc0a9b3919ea6a4a7afaf2157a6a1d943
Mahsa Arabzadeh <arabzadehmah@gmail.com>
2023-09-30 09:34:47 +0330

    add current date

---
+++ assets/JS/script.js
@@ -0,0 +1,4 @@
+// base price of car
+let basePrice = 2000000;
+
+const libraryJalali = requere("jalali-moment");
---
+++ car-insurance-main/js/app.js
@@ -0,0 +1,3 @@
+// get current year.
+const year = new Date().toLocaleDateString("fa-IR");
+console.log(year);
