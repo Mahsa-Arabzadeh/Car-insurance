f2c511c2670709408da34117f6481efa0addb37f
Mahsa Arabzadeh <arabzadehmah@gmail.com>
2023-09-30 17:03:25 +0330

    fix formula of converting (gregorian to jalali)
    Fix the formula for converting the Gregorian year to the jalali year.

--- car-insurance-main/js/app.js
+++ car-insurance-main/js/app.js
@@ -1,16 +1,18 @@
+// Taking the current year and converting it to the jalali year.
 function convertToJalali(georgianYear) {
   // Algorithm to convert Gregorian year to Jalali (Shamsi) year
-  const jalaliYear = ((georgianYear - 621) * 33) / 32;
+  const jalaliYear = georgianYear - 622 + 1;
   return Math.floor(jalaliYear);
 }
 
+// Taking the last 20 years of the current year and showing it in the DOM.
 function displayPast20JalaliYears() {
   // Get the current year in the Jalali calendar system
   const currentYear = convertToJalali(new Date().getFullYear());
 
   const showYear = document.querySelector("#year");
   // Calculate Jalali years from 20 years ago to the current year
-  for (let i = currentYear - 20; i <= currentYear; i++) {
+  for (let i = currentYear; i >= currentYear - 20; i--) {
     // Showing years in DOM.
     showYear.innerHTML += `<option> ${i} </option>`;
   }
