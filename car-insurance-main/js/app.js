85e0c3608b2ebd4ee32068b3731825980b46b695
Mahsa Arabzadeh <arabzadehmah@gmail.com>
2023-09-30 16:17:26 +0330

    show years in DOM
    add options (${i}) in the loop of 20 past year.

--- car-insurance-main/js/app.js
+++ car-insurance-main/js/app.js
@@ -11,9 +11,8 @@ function displayPast20JalaliYears() {
   const showYear = document.querySelector("#year");
   // Calculate Jalali years from 20 years ago to the current year
   for (let i = currentYear - 20; i <= currentYear; i++) {
-    showYear.innerHTML = `
-    <option> ${i} </option>        
-    `;
+    // Showing years in DOM.
+    showYear.innerHTML += `<option> ${i} </option>`;
   }
 }
 // Display 20 years from 1382 to 1402 in the Jalali calendar format
