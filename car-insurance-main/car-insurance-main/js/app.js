8a742d1b9ada181d908b58c31acdaf05810688bf
Mahsa Arabzadeh <arabzadehmah@gmail.com>
2023-10-05 12:25:03 +0330

    add pridePrice into the loop

---
+++ car-insurance-main/car-insurance-main/js/app.js
@@ -0,0 +1,78 @@
+// Variables
+const creatDate = document.querySelector(".creat-date");
+const carModel = document.querySelector(".car-model");
+const carPrice = document.querySelector(".car-price");
+const submitBtn = document.querySelector(".submit");
+const resultFactor = document.querySelector("#result");
+
+// Events
+submitBtn.addEventListener("click", function () {
+  resultFactor.style.display = "block";
+});
+
+// Functions
+// User Interface Functions
+// Show Years
+function displayYears() {
+  // Convert to number
+  let persianNumbers = [
+      /۰/g,
+      /۱/g,
+      /۲/g,
+      /۳/g,
+      /۴/g,
+      /۵/g,
+      /۶/g,
+      /۷/g,
+      /۸/g,
+      /۹/g,
+    ],
+    arabicNumbers = [
+      /٠/g,
+      /١/g,
+      /٢/g,
+      /٣/g,
+      /٤/g,
+      /٥/g,
+      /٦/g,
+      /٧/g,
+      /٨/g,
+      /٩/g,
+    ],
+    fixNumbers = function (str) {
+      if (typeof str === "string") {
+        for (var i = 0; i < 10; i++) {
+          str = str.replace(persianNumbers[i], i).replace(arabicNumbers[i], i);
+        }
+      }
+      return parseInt(str);
+    };
+
+  // get now years
+  let curentYear = new Date().toLocaleDateString("fa-IR");
+
+  // Slice date
+  curentYear = curentYear.slice(0, 4);
+
+  // get max year
+  let maxYear = fixNumbers(curentYear);
+  console.log(maxYear);
+
+  // get min year
+  let minYear = maxYear - 20;
+  console.log(minYear);
+
+  const inputYear = document.querySelector("#year");
+  let bacePrice = 2000000;
+  let pridePrice = bacePrice * 1.15;
+  for (let i = maxYear; i >= minYear; i--) {
+    let optionTag = document.createElement("option");
+    optionTag.value = i;
+    optionTag.innerText = `${i}`;
+    inputYear.appendChild(optionTag);
+    pridePrice *= 0.005;
+    console.log(pridePrice);
+  }
+}
+
+displayYears();
--- car-insurance-main/js/app.js
+++ car-insurance-main/js/app.js
@@ -1,21 +1,58 @@
 // Taking the current year and converting it to the jalali year.
-function convertToJalali(georgianYear) {
-  // Algorithm to convert Gregorian year to Jalali (Shamsi) year
-  const jalaliYear = georgianYear - 622 + 1;
-  return Math.floor(jalaliYear);
-}
+// function convertToJalali(georgianYear) {
+//   // Algorithm to convert Gregorian year to Jalali (Shamsi) year
+//   const jalaliYear = georgianYear - 622 + 1;
+//   return Math.floor(jalaliYear);
+// }
 
-// Taking the last 20 years of the current year and showing it in the DOM.
-function displayPast20JalaliYears() {
-  // Get the current year in the Jalali calendar system
-  const currentYear = convertToJalali(new Date().getFullYear());
+// // Taking the last 20 years of the current year and showing it in the DOM.
+// function displayPast20JalaliYears() {
+//   // Get the current year in the Jalali calendar system
+//   const currentYear = convertToJalali(new Date().getFullYear());
 
-  const showYear = document.querySelector("#year");
-  // Calculate Jalali years from 20 years ago to the current year
-  for (let i = currentYear; i >= currentYear - 20; i--) {
-    // Showing years in DOM.
-    showYear.innerHTML += `<option> ${i} </option>`;
-  }
-}
+//   const showYear = document.querySelector("#year");
+//   // Calculate Jalali years from 20 years ago to the current year
+//   for (let i = currentYear; i >= currentYear - 20; i--) {
+//     // Showing years in DOM.
+//     showYear.innerHTML += `<option> ${i} </option>`;
+//   }
+// }
 // Display 20 years from 1382 to 1402 in the Jalali calendar format
 displayPast20JalaliYears();
+
+function displayYears() {
+  let(
+    (persianNumbers = [
+      /۰/g,
+      /۱/g,
+      /۲/g,
+      /۳/g,
+      /۴/g,
+      /۵/g,
+      /۶/g,
+      /۷/g,
+      /۸/g,
+      /۹/g,
+    ])
+  ),
+    (arabicNumbers = [
+      /٠/g,
+      /١/g,
+      /٢/g,
+      /٣/g,
+      /٤/g,
+      /٥/g,
+      /٦/g,
+      /٧/g,
+      /٨/g,
+      /٩/g,
+    ]),
+    (fixNumbers = function (str) {
+      if (typeof str === "string") {
+        for (var i = 0; i < 10; i++) {
+          str = str.replace(persianNumbers[i], i).replace(arabicNumbers[i], i);
+        }
+        return str;
+      }
+    });
+}
